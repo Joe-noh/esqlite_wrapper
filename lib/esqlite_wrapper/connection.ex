237ecaabff685da3504334fb0fc09dd3914c5885
Joe-noh <goflb.jh@gmail.com>
2014-12-22 07:49:23 +0900

    implement basic wrappers

--- lib/esqlite_wrapper/connection.ex
+++ lib/esqlite_wrapper/connection.ex
@@ -13,6 +13,29 @@ defmodule EsqliteWrapper.Connection do
     GenServer.call(pid, {:query, sql, args})
   end
 
+  def prepare(pid, sql) do
+    GenServer.call(pid, {:prepare, sql})
+  end
+
+  def step(pid, prepared) do
+    GenServer.call(pid, {:step, prepared})
+  end
+
+  def bind(pid, prepared, params) do
+    GenServer.call(pid, {:bind, prepared, params})
+  end
+
+  def column_names(pid, prepared) do
+    GenServer.call(pid, {:column_names, prepared})
+  end
+
+  def close(pid) do
+    GenServer.cast(pid, :close)
+  end
+
+
+  ## GenServer callbacks
+
   def init([path]) do
     path
     |> String.to_char_list
@@ -33,5 +56,31 @@ defmodule EsqliteWrapper.Connection do
       |> :esqlite3.q(args, db)
 
     {:reply, result, db}
+  end
+
+  def handle_call({:prepare, sql}, _from, db) do
+    {:ok, prepared} = sql |> String.to_char_list |> :esqlite3.prepare(db)
+
+    {:reply, prepared, db}
+  end
+
+  def handle_call({:step, prepared}, _from, db) do
+    {:reply, :esqlite3.step(prepared), db}
+  end
+
+  def handle_call({:bind, prepared, params}, _from, db) do
+    {:reply, :esqlite3.bind(prepared, params), db}
+  end
+
+  def handle_call({:column_names, prepared}, _from, db) do
+    {:reply, :esqlite3.column_names(prepared), db}
+  end
+
+  def handle_cast(:close, db) do
+    {:stop, :normal, db}
+  end
+
+  def terminate(_reason, db) do
+    :esqlite3.close(db)
   end
 end
